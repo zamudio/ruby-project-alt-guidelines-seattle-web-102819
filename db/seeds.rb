d3bae808961172e3407d2c637238fdfa83c85cb7
github username <github email address>
2019-11-12 16:38:44 -0800

    Created seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,16 @@
-##
+MusicArtist.destroy_all
+User.destroy_all
+Review.destroy_all
+
+
+10.times do |i|
+    MusicArtist.create(name: Faker::Music.unique.band, genre: Faker::Music.unique.genre, album: Faker::Music.unique.album)
+end
+
+10.times do |i|
+    User.create(name: Faker::Name.unique.name, age: Faker::Number.number(digits: 2), email: Faker::Internet.unique.email)
+end
+
+10.times do |i|
+    Review.create(review: Faker::Lorem.paragraph, rating: Faker::Number.number(digits: 1), user_id: User.all.sample.id, music_artist_id: MusicArtist.all.sample.id)
+end
